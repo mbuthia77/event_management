a53b7b5cf108a260dd9d4d49a7cfae34938d8f72
mbuthia77 <chege.mc@gmail.com>
2024-12-24 11:42:38 +0300

    Update settings.py
    Added import os

--- event_management/settings.py
+++ event_management/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta, datetime
 from django.utils import timezone
@@ -148,7 +149,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
